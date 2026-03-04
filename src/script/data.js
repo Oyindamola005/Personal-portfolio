bcdd8a497ffbb558eaff6131ae037e66dbea7ec0
Oyindamola <damolaoyin005@gmail.com>
2026-03-04 14:41:36 +0100

    wip

--- src/script/data.js
+++ src/script/data.js
@@ -4,28 +4,32 @@ const cardData = [
         alt: "tailor's_diary",
         title: "TAILOR'S DIARY",
         description: "Tailor's Diary is a mobile app designed specifically for tailors and fashion professionals to enhance their measurement-taking process and keeping customer’s details safe and secure.",
-        hashtags: "#UI/UX #Case_study #tailors #tech #mobile_app"
+        hashtags: "#UI/UX #Case_study #tailors #tech #mobile_app",
+        link: "https://oyindamola005.github.io/profile-website/",
     },
     {
         img: "src/assets/the_place.png",
         alt: "the_place",
         title: "PLACE (AR/VR MOBILE APP)",
         description: "PLace is a VR/AR App incooporated into the e-commerce space for users that aim to buy furniture for their homes/ apartments.",
-        hashtags: "#UI/UX #Case_study #VR/AR #tech #mobile_app"
+        hashtags: "#UI/UX #Case_study #VR/AR #tech #mobile_app",
+        link: "https://oyindamola005.github.io/profile-website/"
     },
     {
         img: "src/assets/rugwart.png",
         alt: "rugwart",
         title: "RUGWARTS UNIVERSITY",
         description: "Xender is a mobile App that connects individuals to Dispatch Riders to transport their goods to their destinations.",
-        hashtags: "#UI/UX #Case_study #e-learning #tech #mobile_app"
+        hashtags: "#UI/UX #Case_study #e-learning #tech #mobile_app",
+        link: "https://oyindamola005.github.io/profile-website/",
     },
     {
         img: "src/assets/xender.png",
         alt: "xender",
         title: "XENDER (LOGISTIC MOBILE APP)",
         description: "Xender is a mobile App that connects individuals to Dispatch Riders to transport their goods to their destinations.",
-        hashtags: "#UI/UX #Case_study #e-learning #tech #mobile_app"
+        hashtags: "#UI/UX #Case_study #e-learning #tech #mobile_app",
+        link: "https://oyindamola005.github.io/profile-website/",
     }
 ];
 
